ae02f8d4d0569a902133df7a761d2d77bc470c44
Chris <chris.clark913@gmail.com>
2022-03-28 14:51:40 -0500

    JS functional

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// default library for shaping/styling purposes
-
+// library array and book constructor
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -9,6 +8,19 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// DOM elements for adding a book
+
+const addBookBtn = document.querySelector('#addBookBtn')
+const modal = document.querySelector('.modal')
+addBookBtn.onclick = () => modal.style.display = 'block';
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// create the book from Form data and push book to myLibrary
 function addBookToLibrary() {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
@@ -18,50 +30,19 @@ function addBookToLibrary() {
   const newBook = new Book(title, author, pages, read)
 
   myLibrary.push(newBook)
-
-  displayBooks(newBook)
-
-  console.log(myLibrary)
+  setData();
 }
 
-
-function displayBooks(book) {
-  const booksDiv = document.querySelector('.books');
-    const bookCard = document.createElement('div');
-    booksDiv.appendChild(bookCard).classList.add('book-card');
-
-    const bookTitle = document.createElement('h3');
-    bookTitle.innerHTML = `"${book.title}"`
-    bookCard.appendChild(bookTitle).classList.add('book-title');
-
-    const bookAuthor = document.createElement('p');
-    bookAuthor.innerHTML = `By: ${book.author}`;
-    bookCard.appendChild(bookAuthor).classList.add('book-author');
-
-    const bookPages = document.createElement('p');
-    bookPages.innerHTML = `${book.pages} pages`;
-    bookCard.appendChild(bookPages).classList.add('book-pages');
-
-    const bookRead = document.createElement('button');
-    bookRead.innerHTML = `${book.read}`
-    bookCard.appendChild(bookRead).classList.add('read-btn');
-
-
-    // right now this works to toggle the read status, but it breaks once there's more than one book.
-    const readBtn = document.querySelector('.read-btn');
-    readBtn.addEventListener('click', () => {
-    readBtn.innerHTML = (readBtn.innerHTML == 'Read') ? 'Unread' : 'Read';
-    })
-}
-
-
+// add the book, render the display of books
 const form = document.getElementById('book-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBookToLibrary();
+  render();
   resetForm();
 })
 
+// clear the form after submitting
 function resetForm() {
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
@@ -72,21 +53,76 @@ function resetForm() {
   pages.value = '';
 }
 
-const modal = document.querySelector('.modal')
-const addBookBtn = document.querySelector('#addBookBtn')
-addBookBtn.onclick = () => modal.style.display = 'block';
+// build the bookCard 
+function createBook(book) {
+  const booksDiv = document.querySelector('.books');
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  const bookCard = document.createElement('div');
+  booksDiv.appendChild(bookCard).classList.add('book-card');
+
+  const bookTitle = document.createElement('h3');
+  bookTitle.textContent = `"${book.title}"`
+  bookCard.appendChild(bookTitle).classList.add('book-title');
+
+  const bookAuthor = document.createElement('p');
+  bookAuthor.textContent = `By: ${book.author}`;
+  bookCard.appendChild(bookAuthor).classList.add('book-author');
+  
+  const bookPages = document.createElement('p');
+  bookPages.textContent = `${book.pages} pages`;
+  bookCard.appendChild(bookPages).classList.add('book-pages');
+  
+  // button to toggle read status
+  const bookRead = document.createElement('button');
+  bookRead.textContent = `${book.read}`
+  bookCard.appendChild(bookRead).classList.add('read-btn');
+
+  bookRead.addEventListener('click', () => {
+    bookRead.textContent = (bookRead.textContent == 'Read') ? 'Unread' : 'Read';
+    setData();
+    render();
+  })
+
+  // button to delete each item; removes from display and myLibrary
+  const bookDel = document.createElement('button');
+  bookDel.textContent = 'Delete Book';
+  bookCard.appendChild(bookDel).classList.add('.del-btn');
+
+  bookDel.addEventListener('click', () => {
+    myLibrary.splice(myLibrary.indexOf(book),1);
+    render();
+    setData();
+  })
+}
+
+// create the display for each book
+function render() {
+  const display = document.querySelector('.books');
+  const bookItem = document.querySelectorAll('.book-card')
+  bookItem.forEach(book => display.removeChild(book))
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBook(myLibrary[i]);
   }
 }
 
-// When a user visits the site, there will be a button to add a Book
-// when they click that button, a modal will pop up where they can fill out a form
-// Upon submitting that form, a book object should be created from that input data
-// the book object should populate in the myLibrary
-// each book should display as a card with title, author, pages, read status(toggleable), as well as a delete & edit buttons
+// store/update myLibrary in localstorage
 
-// Secondary:
-// add localstorage usage
+function setData() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+// pull from localstorage, if it exists
+
+function restore() {
+  if(!localStorage.myLibrary) {
+    render()
+  } else {
+    let objects = localStorage.getItem('myLibrary')
+    objects = JSON.parse(objects);
+    myLibrary = objects;
+    render();
+  }
+}
+
+restore();
